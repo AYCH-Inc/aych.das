e1023a6a78c16f757bb393f668615c051aaf3c08
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-03-19 23:39:14 +0700

    Update verify-commits.sh

--- contrib/verify-commits/verify-commits.sh
+++ contrib/verify-commits/verify-commits.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# Copyright (c) 2014-2016 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2014 - 2020 The Bitcoin Core developers
+# Copyright (c) 2019 - 2020 The Aych base Developers
+# Distributed under the AGPL-3.0 and MIT software license, see the accompanying
+# file LICENSE or https://www.gnu.org
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
 DIR=$(dirname "$0")
 [ "/${DIR#/}" != "$DIR" ] && DIR=$(dirname "$(pwd)/$0")
