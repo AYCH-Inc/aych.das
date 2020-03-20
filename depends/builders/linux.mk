81d8e18579706244b58e65fa896788bf200beafe
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-03-20 11:43:20 +0700

    Update linux.mk

--- depends/builders/linux.mk
+++ depends/builders/linux.mk
@@ -1,2 +1,12 @@
 build_linux_SHA256SUM = sha256sum
 build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) -o
+build_linux_scryptsum = scryptsum
+build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) -o
+build_linux_x11sum = x11sum
+build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) -o
+build_linux_aychsum = aychsum
+build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) -o
+build_linux_efssum = efssum
+build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) -o
+build_linux_xrpsum = xrpsum
+build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) -o
